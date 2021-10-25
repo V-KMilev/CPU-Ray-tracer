45f215be2e8d15689ed8894eb723c328ff1b5a77
V-KMIlev <vilimir.k.milev@gmail.com>
2021-10-25 18:46:59 +0300

    OGL buffer fix,  json files removed

--- src/GLFW_window_set.h
+++ src/GLFW_window_set.h
@@ -1,32 +1,49 @@
 #pragma once
 
+/* Future linking for glGenBuffers, glBindBuffer e.t. */
+#define GL_GLEXT_PROTOTYPES
+
 #include <GLFW/glfw3.h>
 
 int window_setup() {
 	GLFWwindow* window;
 
 	/* Initialize the library */
-	if (!glfwInit())
+	if (!glfwInit()) {
 		return -1;
+	}
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "dont open", NULL, NULL);
-	if (!window)
-	{
+
+	if (!window) {
 		glfwTerminate();
 		return -1;
 	}
+
+	unsigned int buffer;
+
+	float positions[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
 	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
+	while (!glfwWindowShouldClose(window)) {
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		/* Swap front and back buffers */
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		/* Swap front and back buffers */ 
 		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
