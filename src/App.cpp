62b75088f16aa9c7cd15e7e6a79dce99698e0cb8
V-KMIlev <vilimir.k.milev@gmail.com>
2022-01-19 18:42:30 +0200

    tiny_obj_loader adder, bug-free (for now)

--- src/App.cpp
+++ src/App.cpp
@@ -15,26 +15,84 @@
 #include <limits>
 #include <iostream>
 
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+
+tinyobj::attrib_t my_attrib;
+std::vector<tinyobj::shape_t> my_shapes;
+std::vector<tinyobj::material_t> my_materials;
+
+static bool myLoadObj(const char* filename, const char* basepath = NULL,
+                        bool triangulate = true) {
+  std::cout << "Loading " << filename << std::endl;
+
+  tinyobj::attrib_t attrib;
+  std::vector<tinyobj::shape_t> shapes;
+  std::vector<tinyobj::material_t> materials;
+
+  std::string warn;
+  std::string err;
+  bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename, basepath, triangulate);
+
+  if (!warn.empty()) {
+    std::cout << "WARN: " << warn << std::endl;
+  }
+
+  if (!err.empty()) {
+    std::cerr << "ERR: " << err << std::endl;
+  }
+
+  if (!ret) {
+    printf("Failed to load/parse .obj.\n");
+    return false;
+  }
+
+  my_attrib = attrib;
+  my_shapes = shapes;
+  my_materials = materials;
+
+  return true;
+  
+}
+
 int main()
 {
+
+  bool ok = myLoadObj("/home/vk/work/Ray-tracer/src/Models/Body_low.obj", "./Models/");
+
+  int size = my_attrib.vertices.size() -1;
+
+
+
   RTCDevice device = rtcNewDevice(NULL);
   RTCScene scene   = rtcNewScene(device);
   RTCGeometry geom = rtcNewGeometry(device, RTC_GEOMETRY_TYPE_TRIANGLE);
 
+
+
   float* vb = (float*) rtcSetNewGeometryBuffer(geom,
-      RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3, 3*sizeof(float), 3);
-  vb[0] = 0.f; vb[1] = 0.f; vb[2] = 0.f; // 1st vertex
-  vb[3] = 1.f; vb[4] = 0.f; vb[5] = 0.f; // 2nd vertex
-  vb[6] = 0.f; vb[7] = 1.f; vb[8] = 0.f; // 3rd vertex
+      RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3, size * sizeof(float), size / 3);
+  for (int i = 0; i < my_attrib.vertices.size(); i++) {
+
+    vb[i * 3] = my_attrib.vertices[i * 3];
+    vb[i * 3 + 1] = my_attrib.vertices[i * 3 + 1];
+    vb[i * 3 + 2] = my_attrib.vertices[i * 3 + 2];
+  }
 
   unsigned* ib = (unsigned*) rtcSetNewGeometryBuffer(geom,
-      RTC_BUFFER_TYPE_INDEX, 0, RTC_FORMAT_UINT3, 3*sizeof(unsigned), 1);
-  ib[0] = 0; ib[1] = 1; ib[2] = 2;
+      RTC_BUFFER_TYPE_INDEX, 0, RTC_FORMAT_UINT3, size / 3 * sizeof(unsigned), size / 3 / 3);
+  for (int i = 0; i < size / 3; i++) {
+    vb[i] = i;
+  }
+
+
 
   rtcCommitGeometry(geom);
   rtcAttachGeometry(scene, geom);
   rtcReleaseGeometry(geom);
   rtcCommitScene(scene);
+
+
 
   RTCRayHit rayhit;
   rayhit.ray.org_x  = 0.25f; rayhit.ray.org_y = 0.25f; rayhit.ray.org_z = -1.f;
@@ -53,6 +111,8 @@ int main()
   } else {
     std::cout << "No Intersection" << std::endl;
   }
+
+
 
   rtcReleaseScene(scene);
   rtcReleaseDevice(device);
